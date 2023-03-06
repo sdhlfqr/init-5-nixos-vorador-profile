71db5df342709dab1c87ebca9f8ad8a7c2fe931a
Sayf Dhulfiqar <126487164+sdhlfqr@users.noreply.github.com>
2023-03-06 17:48:36 +0200

    feat: Move SystemPackages to global

--- system/packages.nix
+++ system/packages.nix
@@ -6,12 +6,4 @@
   nix.gc.automatic = true;
   nix.gc.dates = "weekly";
   nix.gc.options = "--delete-older-than 7d";
-
-  environment.systemPackages = with pkgs; [
-    p7zip
-    unzip
-
-    direnv
-    gnumake
-  ];
 }
